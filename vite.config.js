4048e0a1f9daf853c1648e638f7d278c5d54dd11
Niclette Tshisuaka <nicka20060108@gmail.com>
2025-09-20 22:20:26 +0200

    i have changed the config file to also use the elastic ip

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',   // listen on all network interfaces (makes Elastic IP accessible)
+    port: 5173,         // frontend dev server port (can be changed, just not 3001)
+    strictPort: true,   // if port is taken, Vite will fail instead of using another
     proxy: {
-      "/api": {
-        target: "http://localhost:3001", 
+      '/api': {
+        target: 'http://localhost:3001', // backend server
         changeOrigin: true,
         secure: false,
       },
     },
   },
 });
-
