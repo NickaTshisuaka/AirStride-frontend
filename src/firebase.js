c38e2770afe1518faa4186d0ae520bbe612f88fa
Niclette Tshisuaka <nicka20060108@gmail.com>
2025-11-19 11:52:20 +0200

    i have added the firebase keys to git hub as well

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Use Vite environment variables (prefixed with VITE_)
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
