a637ab96370da1b433b6264484a88bb055348c91
Niclette Tshisuaka <nicka20060108@gmail.com>
2025-11-23 18:25:59 +0200

    sunday8

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -127,10 +127,10 @@ const Checkout = () => {
     };
 
     emailjs.send(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      import.meta.env.service_uztg1lh,
+      import.meta.env.template_1wdwiad,
       templateParams,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      import.meta.env.RIlnlcSIlbll-IpKa
     )
       .then(() => toast.success(`Receipt sent to ${form.email} ✅`))
       .catch((err) => toast.error(`Email failed: ${err.text}`));
